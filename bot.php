35a483e5fcb3d26227ba66e8a6b5b70e9f2ed153
Jirayouth <Jirayouth@users.noreply.github.com>
2017-01-21 15:39:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,7 +43,7 @@ if (!is_null($events['events'])) {
 			
 			// PUSH Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
-			$send_message = $user." ".$userID." : ".$text;
+			$send_message = $user." ".$uid." : ".$text;
 			$messages_push = [
 				'type' => 'text',
 				'text' => $send_message
