90b0d933bc6f732f9facdc600c1aec33896ee08d
Jirayouth <Jirayouth@users.noreply.github.com>
2017-01-21 11:13:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,5 @@
 <?php
-
 $access_token = 'qtfW2whmIBLG4ui5DO1+wIB2vxRW8dUX7t6ksc4ZtYnL6Y/4ZDShhwB0fk03JUkdyStOJ183vPL4oRD9ZEfuHT0eQ4jVw6oBt8+QxNnh4YzFxgSSWoFU+t9JYRBOXk2cwHW0YtzJsQVKVxpOGVhCCgdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,13 +14,13 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+			// Get user
+			$user = $event['source']['text'];
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -31,7 +29,6 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -40,8 +37,14 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
+			
+			$myfile = fopen("nm_list.txt", "w") or die("Unable to open file!");
+			$txt = $user+" : "+$replyToken+"\n";
+			fwrite($myfile, $txt);
+			$txt = "Minnie Mouse\n";
+			fwrite($myfile, $txt);
+			fclose($myfile);
 		}
 	}
 }
