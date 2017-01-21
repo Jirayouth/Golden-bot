4c2a411022a9a1e3fbb42f6aa99824d3d89790f8
Jirayouth <Jirayouth@users.noreply.github.com>
2017-01-21 11:49:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = /*$event['message']['text']+" : "+*/"สวัสดีคุณ ".$event['source']['userId'];
+			$text = /*$event['message']['text']+" : "+*/"สวัสดีคุณ ".$event['source']['groupId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Get user
