eb2c43a027c5e152e3d42b7717288c26795804bb
Jirayouth <Jirayouth@users.noreply.github.com>
2017-12-10 03:33:39 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,8 @@
 <?php
-$access_token = 'qtfW2whmIBLG4ui5DO1+wIB2vxRW8dUX7t6ksc4ZtYnL6Y/4ZDShhwB0fk03JUkdyStOJ183vPL4oRD9ZEfuHT0eQ4jVw6oBt8+QxNnh4YzFxgSSWoFU+t9JYRBOXk2cwHW0YtzJsQVKVxpOGVhCCgdB04t89/1O/w1cDnyilFU=';
+//$access_token = 'qtfW2whmIBLG4ui5DO1+wIB2vxRW8dUX7t6ksc4ZtYnL6Y/4ZDShhwB0fk03JUkdyStOJ183vPL4oRD9ZEfuHT0eQ4jVw6oBt8+QxNnh4YzFxgSSWoFU+t9JYRBOXk2cwHW0YtzJsQVKVxpOGVhCCgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'rB8mTotb8cqebrYNvDAwDwrKoePdRrM7f1q2gf93pAE4iplt1mnZ4UhD1/8yS9twsyM0ix5/01thdx3qM1ksKwTfATwQ+09gtoQ7wArCoKzpz2KkwFiQuT0C4D5irLZqkkXDIYsGnGLFaRTIe6DIzAdB04t89/1O/w1cDnyilFU=';
 
+//$url = 'https://api.line.me/v1/oauth/verify';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
